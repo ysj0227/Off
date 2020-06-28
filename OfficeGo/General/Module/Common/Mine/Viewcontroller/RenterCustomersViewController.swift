7b6fed1d07ed5d8a8b98e0ad412cf394cf44f730
mac <995924226@qq.com>
2020-06-28 17:03:56 +0800

    我的 -  客服 - 邮件发送 -

--- OfficeGo/General/Module/Common/Mine/Viewcontroller/RenterCustomersViewController.swift
+++ OfficeGo/General/Module/Common/Mine/Viewcontroller/RenterCustomersViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import MessageUI
 
-class RenterCustomersViewController: BaseViewController {
+class RenterCustomersViewController: BaseViewController, MFMailComposeViewControllerDelegate {
 
     lazy var bgImgView: UIImageView = {
         let view = UIImageView()
@@ -74,9 +75,63 @@ class RenterCustomersViewController: BaseViewController {
 
     
     @objc func emailClick() {
-        
+        //0.首先判断设备是否能发送邮件
+        if MFMailComposeViewController.canSendMail() {
+            //1.配置邮件窗口
+            let mailView = configuredMailComposeViewController()
+            //2. 显示邮件窗口
+            present(mailView, animated: true, completion: nil)
+        } else {
+            print("Whoop...设备不能发送邮件")
+            showSendMailErrorAlert()
+        }
     }
-    
+    //MARK:- helper methods
+    //配置邮件窗口
+    func configuredMailComposeViewController() -> MFMailComposeViewController {
+
+        let mailComposeVC = MFMailComposeViewController()
+        mailComposeVC.mailComposeDelegate = self
+
+        //设置邮件地址、主题及正文
+        mailComposeVC.setToRecipients(["Clientservice@officego.com"])
+//        mailComposeVC.setToRecipients(["995924226@qq.com"])
+
+        mailComposeVC.setSubject("")
+        mailComposeVC.setMessageBody("", isHTML: false)
+
+        return mailComposeVC
+    }
+
+
+    //提示框，提示用户设置邮箱
+    func showSendMailErrorAlert() {
+
+        let sendMailErrorAlert = UIAlertController(title: "未开启邮件功能", message: "设备邮件功能尚未开启，请在设置中更改", preferredStyle: .alert)
+        sendMailErrorAlert.addAction(UIAlertAction(title: "确定", style: .default) { _ in })
+        self.present(sendMailErrorAlert, animated: true){}
+    }
+
+
+    //MARK:- Mail Delegate
+    //用户退出邮件窗口时被调用
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        switch result.rawValue{
+        case MFMailComposeResult.sent.rawValue:
+            print("邮件已发送")
+        case MFMailComposeResult.cancelled.rawValue:
+            print("邮件已取消")
+        case MFMailComposeResult.saved.rawValue:
+            print("邮件已保存")
+        case MFMailComposeResult.failed.rawValue:
+            print("邮件发送失败")
+        default:
+            print("邮件没有发送")
+            break
+        }
+
+        controller.dismiss(animated: true, completion: nil)
+    }
     @objc func phoneClick() {
         let alertController = UIAlertController.init(title: "联系客服", message: nil, preferredStyle: .actionSheet)
         let refreshAction = UIAlertAction.init(title: "业务咨询", style: .default) { (action: UIAlertAction) in
