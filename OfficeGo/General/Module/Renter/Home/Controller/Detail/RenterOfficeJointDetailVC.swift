d5d8d184afeee96a2b5296c7788cd7e499c00a5e
mac <995924226@qq.com>
2020-07-03 18:53:51 +0800

    分享地址修改

--- OfficeGo/General/Module/Renter/Home/Controller/Detail/RenterOfficeJointDetailVC.swift
+++ OfficeGo/General/Module/Renter/Home/Controller/Detail/RenterOfficeJointDetailVC.swift
@@ -98,7 +98,7 @@ class RenterOfficeJointDetailVC: BaseTableViewController, WMPlayerDelegate {
         shareVC.buildingName = buildingDetailViewModel?.buildingViewModel?.buildingName ?? ""
         shareVC.descriptionString = buildingDetailViewModel?.buildingViewModel?.addressString ?? ""
         shareVC.thumbImage = buildingDetailViewModel?.buildingViewModel?.mainPic
-        shareVC.shareUrl = "\(SSAPI.SSH5Host)\(SSDelegateURL.h5BuildingDetailShareUrl)?buildingId=\(buildingDetailViewModel?.buildingViewModel?.buildingId ?? 0)"
+        shareVC.shareUrl = "\(SSAPI.SSH5Host)\(SSDelegateURL.h5JointDetailShareUrl)?buildingId=\(buildingDetailViewModel?.buildingViewModel?.buildingId ?? 0)"
         shareVC.modalPresentationStyle = .overFullScreen
         self.present(shareVC, animated: true, completion: {})
     }
--- OfficeGo/General/Module/Renter/Home/Controller/Detail/RenterOfficeJointFYDetailVC.swift
+++ OfficeGo/General/Module/Renter/Home/Controller/Detail/RenterOfficeJointFYDetailVC.swift
@@ -58,7 +58,7 @@ class RenterOfficeJointFYDetailVC: BaseTableViewController {
         shareVC.buildingName = buildingFYDetailViewModel?.houseViewModel?.buildingName ?? ""
         shareVC.descriptionString = buildingFYDetailViewModel?.houseViewModel?.addressString ?? ""
         shareVC.thumbImage = buildingFYDetailViewModel?.houseViewModel?.mainPic
-        shareVC.shareUrl = "\(SSAPI.SSH5Host)\(SSDelegateURL.h5BuildingFYDetailShareUrl)?buildingId=\(buildingFYDetailViewModel?.houseViewModel?.buildingId ?? 0)&houseId=\(buildingFYDetailModel?.house?.id ?? 0)"
+        shareVC.shareUrl = "\(SSAPI.SSH5Host)\(SSDelegateURL.h5BJointFYDetailShareUrl)?buildingId=\(buildingFYDetailViewModel?.houseViewModel?.buildingId ?? 0)&houseId=\(buildingFYDetailModel?.house?.id ?? 0)"
         shareVC.modalPresentationStyle = .overFullScreen
         self.present(shareVC, animated: true, completion: {})
     }
