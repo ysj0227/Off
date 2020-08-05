aa9ddddb1dd11d8fdce529fcaeffa5fe7909a769
mac <995924226@qq.com>
2020-08-05 19:35:16 +0800

    添加tabbar行程图标

---
+++ OfficeGo/General/Module/Owner/Identification/Basic/Controller/OwnerEnterCreatedCompanyViewController.swift
@@ -0,0 +1,21 @@
+//
+//  OwnerEnterCreatedCompanyViewController.swift
+//  OfficeGo
+//
+//  Created by mac on 2020/8/5.
+//  Copyright © 2020 Senwei. All rights reserved.
+//
+
+import UIKit
+
+class OwnerEnterCreatedCompanyViewController: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
