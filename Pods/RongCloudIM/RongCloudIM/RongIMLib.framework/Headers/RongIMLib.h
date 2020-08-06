2d29b09ab738ff608bb75ab84193235d545d6467
KeKe <sxsdjkk@foxmail.com>
2020-08-06 23:31:56 +0800

    rcim 2.10.5更新

--- Pods/RongCloudIM/RongCloudIM/RongIMLib.framework/Headers/RongIMLib.h
+++ Pods/RongCloudIM/RongCloudIM/RongIMLib.framework/Headers/RongIMLib.h
@@ -55,6 +55,8 @@ FOUNDATION_EXPORT const unsigned char RongIMLibVersionString[];
 #import <RongIMLib/RCVoiceMessage.h>
 #import <RongIMLib/RCHQVoiceMessage.h>
 #import <RongIMLib/RCSightMessage.h>
+#import <RongIMLib/RCChatroomKVNotificationMessage.h>
+#import <RongIMLib/RCReferenceMessage.h>
 /// 工具类
 #import <RongIMLib/RCAMRDataConverter.h>
 #import <RongIMLib/RCTSMutableDictionary.h>
@@ -87,6 +89,6 @@ FOUNDATION_EXPORT const unsigned char RongIMLibVersionString[];
 // log
 #import <RongIMLib/RCFwLog.h>
 
-//Downlad
+// Downlad
 #import <RongIMLib/RCDownloadItem.h>
 #import <RongIMLib/RCResumeableDownloader.h>
