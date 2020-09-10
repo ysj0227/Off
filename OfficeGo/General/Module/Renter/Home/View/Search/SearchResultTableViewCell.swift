4809840a01324974316f55d0b062117d434f2106
mac <995924226@qq.com>
2020-09-10 18:54:30 +0800

    搜索图标修改

--- OfficeGo/General/Module/Renter/Home/View/Search/SearchResultTableViewCell.swift
+++ OfficeGo/General/Module/Renter/Home/View/Search/SearchResultTableViewCell.swift
@@ -20,7 +20,7 @@ class SearchResultTableViewCell: BaseTableViewCell {
     lazy var houseDictrictIcon: UIImageView = {
         let view = UIImageView()
         view.contentMode = .scaleAspectFit
-        view.image = UIImage.init(named: "trafficIcon")
+        view.image = UIImage.init(named: "searchAddress")
         return view
     }()
     lazy var houseDistrictBusinessLabel: UILabel = {
