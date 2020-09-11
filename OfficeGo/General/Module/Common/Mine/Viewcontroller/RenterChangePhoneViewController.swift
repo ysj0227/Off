4414a4ef8c6a30827616914765dd41d032614031
mac <995924226@qq.com>
2020-09-11 16:55:40 +0800

    手机号提示

--- OfficeGo/General/Module/Common/Mine/Viewcontroller/RenterChangePhoneViewController.swift
+++ OfficeGo/General/Module/Common/Mine/Viewcontroller/RenterChangePhoneViewController.swift
@@ -253,6 +253,11 @@ extension RenterChangePhoneViewController {
             return
         }
         
+        if SSTool.isPhoneNumber(phoneNumber: phoneNewField.text ?? "") != true {
+            AppUtilities.makeToast("请输入正确的手机号")
+            return
+        }
+        
         if verifyCodeField.text?.isBlankString ?? false {
             AppUtilities.makeToast("请输入验证码")
             return
