f24ade3b57a8a9f484356ff8aedac291ccf3b189
gbzjl <995924226@qq.com>
2020-11-05 11:33:37 +0800

    楼盘网点添加体魄选择处理

--- OfficeGo/General/Module/Owner/Home/Controller/OwnerBuildingJointCreatAddViewController.swift
+++ OfficeGo/General/Module/Owner/Home/Controller/OwnerBuildingJointCreatAddViewController.swift
@@ -494,7 +494,7 @@ extension OwnerBuildingJointCreatAddViewController {
 extension OwnerBuildingJointCreatAddViewController {
     func selectFCZPicker() {
         var imgArr = [BannerModel]()
-        fczImagePickTool.cl_setupImagePickerWith(MaxImagesCount: ownerMaxFCZNumber_4 - uploadPicModelFCZArr.count) {[weak self] (asset,cutImage) in
+        fczImagePickTool.cl_setupImagePickerWith(MaxImagesCount: ownerMaxFCZNumber_4 - uploadPicModelFCZArr.count, superVC: self) {[weak self] (asset,cutImage) in
             // 内部提供的方法可以异步获取图片，同步获取的话时间比较长，不建议！，如果是iCloud中的照片就直接从icloud中下载，下载完成后返回图片,同时也提供了下载失败的方法
             CLImagePickerTool.convertAssetArrToOriginImage(assetArr: asset, scale: 0.1, successClouse: {[weak self] (image,assetItem) in
                 let img = image.resizeMax1500Image()
@@ -514,7 +514,7 @@ extension OwnerBuildingJointCreatAddViewController {
         
     func selectMainPagePicker() {
         
-        mainPicImagePickTool.cl_setupImagePickerWith(MaxImagesCount: 1) {[weak self] (asset,cutImage) in
+        mainPicImagePickTool.cl_setupImagePickerWith(MaxImagesCount: 1, superVC: self) {[weak self] (asset,cutImage) in
             // 内部提供的方法可以异步获取图片，同步获取的话时间比较长，不建议！，如果是iCloud中的照片就直接从icloud中下载，下载完成后返回图片,同时也提供了下载失败的方法
             CLImagePickerTool.convertAssetArrToOriginImage(assetArr: asset, scale: 0.1, successClouse: {[weak self] (image,assetItem) in
                 let img = image.resizeMax1500Image()
