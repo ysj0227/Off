fabc300393d7c7f531b7a5ff0b459cf2e6046f2d
gbzjl <995924226@qq.com>
2020-11-13 21:41:38 +0800

    展示修改

--- OfficeGo/General/Module/Owner/Home/View/OwnerBuildingNetworkSelectCell.swift
+++ OfficeGo/General/Module/Owner/Home/View/OwnerBuildingNetworkSelectCell.swift
@@ -270,7 +270,7 @@ extension OwnerBuildingNetworkSelectCell: UICollectionViewDataSource, UICollecti
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: (kWidth - left_pending_space_17 * 2 - 10 * 2) / 3, height: 40)
+        return CGSize(width: (kWidth - left_pending_space_17 * 2 - 10 * 2 - 5) / 3, height: 40)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
