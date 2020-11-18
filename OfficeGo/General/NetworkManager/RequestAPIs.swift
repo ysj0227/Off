fb8922ae37eb0a175f16c4f8fb59e207605091e4
gbzjl <995924226@qq.com>
2020-11-18 14:21:53 +0800

    接口修改

--- OfficeGo/General/NetworkManager/RequestAPIs.swift
+++ OfficeGo/General/NetworkManager/RequestAPIs.swift
@@ -191,7 +191,7 @@ class SSChatURL: NSObject {
     static let addWxID = "api/user/changeWxId"
     
     //记录第一次发送
-    static let addChatApp = "api/chat/addChatApp"
+    static let addChatApp = "api/chat/addChat"
     
     //获取融云用户信息
     static let getUserChatInfoApp = "api/user/getUser"
